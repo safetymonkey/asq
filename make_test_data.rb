e6704cf806da346f3d4fd57880cdab6edddc4371
Greg Mertzlufft <greg@bikebloc.com>
2017-02-06 21:30:58 -0800

    file cleanup

--- make_test_data.rb
+++
@@ -1,8 +0,0 @@
-asq = Asq.new(:name => "Test asq", :query => "select * from test;")
-asq.save
-
-database = Database.new(:name => "PageDB", :hostname => "pagedb1.sad.marchex.com", :db_type => "mysql")
-database.save
-
-database = Database.new(:name => "FortKnoxDB", :hostname => "fortknoxb1.sad.marchex.com", :db_type => "postgres")
-database.save

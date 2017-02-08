d1230585136174462f71bc401165f8e4e45385ca
Jon Grover <jgrover@marchex.com>
2017-02-08 09:03:24 -0800

    Update session & registration form views to match Bootstrap style.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,12 @@ class RegistrationsController < Devise::RegistrationsController
   before_action :update_sanitized_params, if: :devise_controller?
 
   def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:login, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:login, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:login, :email, :password, :password_confirmation, :remember_me)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:login, :email, :password, :password_confirmation,
+               :current_password)
+    end
   end
 end
